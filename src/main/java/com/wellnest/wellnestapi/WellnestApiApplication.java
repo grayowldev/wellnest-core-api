b38807653103193ffa0ea6f32ff7ed4e8467f504
kwasinsiah <truegrayowl@gmail.com>
2024-10-14 11:22:29 -0400

    initialized project

---
+++ src/main/java/com/wellnest/wellnestapi/WellnestApiApplication.java
@@ -0,0 +1,13 @@
+package com.wellnest.wellnestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellnestApiApplication.class, args);
+    }
+
+}
